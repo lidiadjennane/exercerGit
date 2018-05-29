ab6c0d25d42aeeeca4d6be46962a92dbc978d4a9
lidia <djennane.lidia@gmail.com>
2018-05-29 16:33:35 +0200

    j'ai ajouté un commentaire dans EC2list

--- EC2List.py
+++ EC2List.py
@@ -2,6 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2') 
 instance = ec2.instances.all()
 #Ce commentaire a été ajouté ******************************
+# Ceci est un commentaire pour github
 #List of running instances , their types and state 
 for i in instance:
 	if (i.state["Code"] == 16): 
